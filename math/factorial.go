b78cef4e577569d6639b6dad34cd539b060bfccc
Wayan Jimmy <jimmyeatcrab@gmail.com>
2017-01-08 23:40:44 +0800

    Factorial recursive method

--- math/factorial.go
+++ math/factorial.go
@@ -1,6 +1,17 @@
+/*
+What is a factorial? I think an example is better than an explaination in
+this case:
+
+Factorial(4)
+4 * Factorial(3)
+4 * 3 * Factorial(2)
+4 * 3 * 2 * Factorial(1)
+4 * 3 * 2 * 1
+*/
+
 package math
 
-// FactorialIterative Count factorial by iterative method
+// FactorialIterative count factorial by iterative method
 func FactorialIterative(num int) int {
 	factorial, current := 1, 1
 	for current < num {
@@ -8,4 +19,12 @@ func FactorialIterative(num int) int {
 		factorial *= current
 	}
 	return factorial
+}
+
+// FactorialRecursive count factorial by recursive method
+func FactorialRecursive(num int) int {
+	if num == 1 {
+		return num
+	}
+	return num * FactorialIterative(num-1)
 }
--- math/factorial_test.go
+++ math/factorial_test.go
@@ -4,22 +4,36 @@ import "testing"
 
 func TestFactorialIterative(t *testing.T) {
 	if FactorialIterative(1) != 1 {
-		t.Error("Expected 6")
+		t.Error("Expected 1")
 	}
-
 	if FactorialIterative(2) != 2 {
 		t.Error("Expected 2")
 	}
-
 	if FactorialIterative(3) != 6 {
 		t.Error("Expected 6")
 	}
-
 	if FactorialIterative(4) != 24 {
 		t.Error("Expected 24")
 	}
-
 	if FactorialIterative(10) != 3628800 {
+		t.Error("Expected 3628800")
+	}
+}
+
+func TestFactorialRecursive(t *testing.T) {
+	if FactorialRecursive(1) != 1 {
+		t.Error("Expected 1")
+	}
+	if FactorialRecursive(2) != 2 {
+		t.Error("Expected 2")
+	}
+	if FactorialRecursive(3) != 6 {
+		t.Error("Expected 6")
+	}
+	if FactorialRecursive(4) != 24 {
+		t.Error("Expected 24")
+	}
+	if FactorialRecursive(10) != 3628800 {
 		t.Error("Expected 3628800")
 	}
 }
